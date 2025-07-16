572b02b4ef11c999170e55ed615ba532df723596
7Zaina <z.hadjhamou@gmail.com>
2025-07-16 14:51:46 +0200

    detail produit

---
+++ cypress/e2e/detailproduit.cy.js
@@ -0,0 +1,21 @@
+///<reference types="cypress"/>
+
+describe('afficher detail produit',()=>{
+    beforeEach(()=>{
+        cy.visit('https://www.saucedemo.com');
+    }),
+     it("detail produit Backpack",()=>{
+        cy.get('#user-name').type("standard_user");
+        cy.get('#password').type("secret_sauce");
+        cy.get('#login-button').click();
+        
+});
+     it("detail produit Backpack",()=>{
+        cy.get('.inventory_item_name ').contains('Sauce Labs Backpack').click();
+        cy.get('back-to-products').should('be.visible')
+     });
+     it("detail produit Backpack",()=>{
+        cy.get('.inventory_item_name ').contains('Sauce Labs Bike Light').click();
+        cy.get('back-to-products').should('be.visible')
+     });
+});
